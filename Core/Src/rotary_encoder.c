159ff9b18020d640c29c8a12b09ba0555f82f0a7
Bruno Malli <bruno.malli@ossiaco.com>
2021-04-11 22:30:17 -0400

    Rotary encode code cleanup

--- Core/Src/rotary_encoder.c
+++ Core/Src/rotary_encoder.c
@@ -7,14 +7,14 @@
 #include "rotary_encoder.h"
 #include <stdbool.h>
 
+#define DEBOUNCE_TIME 3 // ms
+
 typedef enum {
 	ROTARY_PIN_A,
 	ROTARY_PIN_B
 }RotaryPin;
 
 RotaryEncoderCtx rotaryEncoder;
-
-int8_t lookup_table[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
 
 void rotaryEncoderInit(RotaryGpioPort portA, RotaryGpioPin pinA, RotaryGpioPort portB, RotaryGpioPin pinB, RotaryGpioPort portSw, RotaryGpioPin pinSw,
 						void (*incrementHandler)(void), void (*decrementHandler)(void))
@@ -29,71 +29,20 @@ void rotaryEncoderInit(RotaryGpioPort portA, RotaryGpioPin pinA, RotaryGpioPort 
 	rotaryEncoder.onDecrement = decrementHandler;
 }
 
-bool up = false, down = false; uint16_t state = 0;
 void rotartEncoderIsr(void)
 {
-
-
-#if 0
-	if(currentA != rotaryEncoder.lastApin)
-	{
-		RotaryPinState currentB = HAL_GPIO_ReadPin(rotaryEncoder.bPort, rotaryEncoder.bPin);
-		if(currentA != currentB)
-		{
-			if(up)
-			{
-				if(rotaryEncoder.onIncrement != NULL)
-					rotaryEncoder.onIncrement();
-				rotaryEncoder.count++;
-				up = false;
-			}
-			else
-				up = true;
-		}
-		else
-		{
-			if(down)
-			{
-				if(rotaryEncoder.onDecrement != NULL)
-					rotaryEncoder.onDecrement();
-				rotaryEncoder.count--;
-				down = false;
-			}
-			else
-				down = true;
-		}
-
-		rotaryEncoder.lastApin = currentA;
-	}
-#else
-//	RotaryPinState currentB = HAL_GPIO_ReadPin(rotaryEncoder.bPort, rotaryEncoder.bPin);
-//	if(currentA != currentB)
-//	{
-//		if(rotaryEncoder.onIncrement != NULL)
-//			rotaryEncoder.onIncrement();
-//		rotaryEncoder.count++;
-//	}
-//	else
-//	{
-//		if(rotaryEncoder.onDecrement != NULL)
-//			rotaryEncoder.onDecrement();
-//		rotaryEncoder.count--;
-//	}
-
 	if((HAL_GPIO_ReadPin(rotaryEncoder.aPort, rotaryEncoder.aPin) == GPIO_PIN_RESET) && !rotaryEncoder.isDebounce)
 	{
 		rotaryEncoder.isDebounce = true;
 		rotaryEncoder.debounce.startTicks = HAL_GetTick();
 	}
-
-#endif
 }
 
 void rotaryEncoderTask(void)
 {
 	if(rotaryEncoder.isDebounce)
 	{
-		if((HAL_GetTick() - rotaryEncoder.debounce.startTicks) >= 3)
+		if((HAL_GetTick() - rotaryEncoder.debounce.startTicks) >= DEBOUNCE_TIME)
 		{
 			rotaryEncoder.isDebounce = false;
 			RotaryPinState currentA = HAL_GPIO_ReadPin(rotaryEncoder.aPort, rotaryEncoder.aPin);
