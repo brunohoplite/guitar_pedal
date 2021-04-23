00d4662ee8733af4568bffb1628adc6cc428b097
Bruno Malli <bruno.malli@ossiaco.com>
2021-04-22 21:31:56 -0400

    Delay gets struct

--- Core/Src/delay.c
+++ Core/Src/delay.c
@@ -7,30 +7,40 @@
 #include "delay.h"
 
 #define MAX_DELAY_BUFFER 4000
-int32_t guitarBuffer[MAX_DELAY_BUFFER] = { 0 };
-unsigned i = 0, j = 1;
 
-signed delayLevel = 6;
+typedef struct {
+	int32_t guitarBuffer[MAX_DELAY_BUFFER];
+	unsigned i;
+	unsigned j;
+	signed delayLevel;
+}DelayEffect;
+
+DelayEffect delayCtx = {
+		.guitarBuffer = { 0 },
+		.i = 0,
+		.j = 1,
+		.delayLevel = 6
+};
 
 uint32_t delay(uint32_t guitarOut, uint32_t dcBias)
 {
 	int32_t unBiased = guitarOut - dcBias;
 
-	int32_t delayed = ((delayLevel * guitarBuffer[j])) / 15;
+	int32_t delayed = ((delayCtx.delayLevel * delayCtx.guitarBuffer[delayCtx.j])) / 15;
 	int32_t out = unBiased + delayed;
-	guitarBuffer[i] = out;
-	if(++i >= MAX_DELAY_BUFFER) i = 0;
-	if(++j >= MAX_DELAY_BUFFER) j = 0;
+	delayCtx.guitarBuffer[delayCtx.i] = out;
+	if(++delayCtx.i >= MAX_DELAY_BUFFER) delayCtx.i = 0;
+	if(++delayCtx.j >= MAX_DELAY_BUFFER) delayCtx.j = 0;
 
 	return out + dcBias;
 }
 
 const signed getDelayLevelValue(void)
 {
-	return delayLevel;
+	return delayCtx.delayLevel;
 }
 
 signed* getDelayLevel(void)
 {
-	return &delayLevel;
+	return &delayCtx.delayLevel;
 }
