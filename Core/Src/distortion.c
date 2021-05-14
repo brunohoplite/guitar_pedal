7c6f524658bdddc861f061521fe176bb5f91a6f5
Bruno Malli <bruno.malli@ossiaco.com>
2021-05-13 22:20:23 -0400

    Distortion hard clipping

--- Core/Src/distortion.c
+++ Core/Src/distortion.c
@@ -8,12 +8,14 @@
 #include "distortion.h"
 
 unsigned gain = 10;
+float boost = 2;
+float divider = 248.f;
 
 uint32_t distortion(uint32_t guitarOut, uint32_t dcBias)
 {
 	float out = (float)guitarOut - (float)dcBias;
-
-	out /= 186.f;
+#if 1
+	out /= divider;
 	float sign;
 	if(out < 0)
 		sign = -1.f;
@@ -25,8 +27,29 @@ uint32_t distortion(uint32_t guitarOut, uint32_t dcBias)
 	out *= (float)gain;
 	float distorted = 1 - exp(out);
 	distorted *= sign;
+	distorted *= boost;
 
-	return (uint32_t)((distorted) * 186.f)+ dcBias;
+	return (uint32_t)((distorted) * divider) + dcBias;
+#else
+	if(1)
+	{
+		if(out > 100)
+		{
+			float distorted = 100;
+			return (uint32_t)distorted + dcBias;
+		}
+		else if(out < -100.f)
+		{
+			float distorted = -100.f;
+			distorted += (float)dcBias;
+			return (uint32_t)distorted;
+		}
+		else
+			return out + dcBias;
+	}
+	else
+		return out + dcBias;
+#endif
 }
 
 const unsigned getDistortionGainValue(void)
