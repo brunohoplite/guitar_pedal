b9e1ea860b21902e71034b44db6351f6eb962c61
Bruno Malli <bruno.malli@ossiaco.com>
2021-10-02 21:52:39 -0400

    Adds CMSIS DSP

---
+++ Core/Inc/tuner.h
@@ -0,0 +1,13 @@
+/*
+ * tuner.h
+ *
+ *  Created on: Sep 11, 2021
+ *      Author: bruno
+ */
+
+#ifndef INC_TUNER_H_
+#define INC_TUNER_H_
+
+#include "main.h"
+
+#endif /* INC_TUNER_H_ */
---
+++ Core/Src/tuner.c
@@ -0,0 +1,17 @@
+/*
+ * tuner.c
+ *
+ *  Created on: Sep 11, 2021
+ *      Author: bruno
+ */
+#include "tuner.h"
+
+#define LOW_E_FREQ	82.41f
+#define A_FREQ		110.0f
+#define D_FREQ		146.83f
+#define G_FREQ		196.0f
+#define B_FREQ		246.94f
+#define E_FREQ		329.63f
+
+
+
