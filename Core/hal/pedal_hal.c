aa3c0aa031e18526361268d6ae76f528a8ae90dc
Bruno <bruno.malli@dcbel.energy>
2022-12-24 19:11:49 -0500

    Tab becomes 4 spaces

--- Core/hal/pedal_hal.c
+++ Core/hal/pedal_hal.c
@@ -13,11 +13,11 @@ uint32_t outputBuffer[DOUBLE_BUFFER_SIZE];
 /*********************************************** Pedal HAL data structure *********************************************************/
 
 typedef struct {
-	ADC_HandleTypeDef* adc;
-	DAC_HandleTypeDef* dac;
-	uint32_t* in;
-	uint32_t* out;	
-	bool isDataReady;
+    ADC_HandleTypeDef* adc;
+    DAC_HandleTypeDef* dac;
+    uint32_t* in;
+    uint32_t* out;
+    bool isDataReady;
 }PedalHal;
 
 PedalHal ctx = { 0 };
@@ -26,54 +26,54 @@ PedalHal ctx = { 0 };
 
 static void pedalHalStartAdcDma(void)
 {
-	HAL_ADC_Start_DMA(ctx.adc, &inputBuffer[0], DOUBLE_BUFFER_SIZE);
+    HAL_ADC_Start_DMA(ctx.adc, &inputBuffer[0], DOUBLE_BUFFER_SIZE);
 }
 
 void pedalHalInit(ADC_HandleTypeDef* adc, DAC_HandleTypeDef* dac)
 {
     ctx.adc = adc;
-	ctx.dac = dac;
-	pedalHalStartAdcDma();
+    ctx.dac = dac;
+    pedalHalStartAdcDma();
 }
 
 void pedalHalProcessData(void)
 {
-	if (ctx.isDataReady)
-	{
-		for (unsigned i = 0; i < (DOUBLE_BUFFER_SIZE / 2); i++)
+    if (ctx.isDataReady)
+    {
+        for (unsigned i = 0; i < (DOUBLE_BUFFER_SIZE / 2); i++)
         {
             pedalOutputTask(&ctx.in[i], &ctx.out[i]);
         }
-		ctx.isDataReady = false;
-	}
+        ctx.isDataReady = false;
+    }
 }
 
 /**************************************************** ST HAL Callbacks ***************************************************************/
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-	if(hadc == ctx.adc)
-	{
-		ctx.in = &inputBuffer[DOUBLE_BUFFER_SIZE / 2];
-		ctx.out = &outputBuffer[DOUBLE_BUFFER_SIZE / 2];
-		ctx.isDataReady = true;
-	}
+    if(hadc == ctx.adc)
+    {
+        ctx.in = &inputBuffer[DOUBLE_BUFFER_SIZE / 2];
+        ctx.out = &outputBuffer[DOUBLE_BUFFER_SIZE / 2];
+        ctx.isDataReady = true;
+    }
 }
 
 void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
 {
-	if(hadc == ctx.adc)
-	{
-		ctx.in = &inputBuffer[0];
-		ctx.out = &outputBuffer[0];
-		ctx.isDataReady = true;
-	}
+    if(hadc == ctx.adc)
+    {
+        ctx.in = &inputBuffer[0];
+        ctx.out = &outputBuffer[0];
+        ctx.isDataReady = true;
+    }
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	if(GPIO_Pin == ROTARY_SW_Pin)
-		swEncoderIsr();
-	else if(GPIO_Pin == ROTARY_A_Pin)
-		rotaryEncoderIsr();
+    if(GPIO_Pin == ROTARY_SW_Pin)
+        swEncoderIsr();
+    else if(GPIO_Pin == ROTARY_A_Pin)
+        rotaryEncoderIsr();
 }
