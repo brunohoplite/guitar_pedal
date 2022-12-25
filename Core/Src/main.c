8645b5ef13d1bc006ab9f1c55b8f4e393ef0282b
brunohoplite <bruno.malli@live.com>
2022-12-24 20:47:41 -0500

    Adjusts timer period

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -343,7 +343,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 3;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 492;
+  htim2.Init.Period = 490;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
